6b7c07bc810750794b9e4548c0bb05868cf03466
chon26909 <chon26909@gmail.com>
2021-08-27 22:47:02 +0700

    first commit

---
+++ pages/posts/create.vue
@@ -0,0 +1,17 @@
+<template>
+  <FormCreate/>
+</template>
+
+<script>
+import FormCreate from '@/components/admin/FormCreate';
+export default {
+    components:{
+        FormCreate
+    }
+
+}
+</script>
+
+<style>
+
+</style>
